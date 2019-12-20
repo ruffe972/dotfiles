8b659c45eaa3b6b11fd5cd96986716e6903dc5d0
Ivan Ershov <ivanershov51@gmail.com>
2019-12-20 16:41:11 +0300

    remove latex

--- config.fish
+++ config.fish
@@ -2,9 +2,17 @@ if not status --is-interactive
     exit 0
 end
 
-alias chmrf='find . -type f -print0 | xargs -0 chmod 0644'
-alias chmrd='find . -type d -print0 | xargs -0 chmod 0755'
-alias chmr='chrf;chmrd'
+# chmod
+alias set-def-file-perm-rec='find . -type f -print0 | xargs -0 chmod 0644'
+alias set-def-dir-perm-rec='find . -type d -print0 | xargs -0 chmod 0755'
+alias set-def-perm-rec='set-def-file-perm-rec;set-def-dir-perm-rec'
+
+# git
+alias gch='git checkout'
+alias gst='git status'
+alias gbr='git branch'
+alias gsh='git stash'
+alias gcm='git commit'
 
 set local_fish_config ~/.config/fish/local_config.fish
 if test -f $local_fish_config
