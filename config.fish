f51be038bdb1152a35a9bc400d85119c8c84bbe8
Ivan Ershov <ivanershov51@gmail.com>
2019-12-24 19:49:14 +0300

    work

--- config.fish
+++ config.fish
@@ -14,7 +14,7 @@ alias gbr='git branch'
 alias gsh='git stash'
 alias gcm='git commit'
 
-set -gx VISUAL "code"
+set -gx VISUAL micro
 set -gx EDITOR $VISUAL
 
 if test (uname) = Linux
@@ -27,4 +27,18 @@ if test (uname) = Linux
     # my-rsync remote_host_name:remote_dir/ local_dir
     # Slash is important! It means "Copy not the dir itself, but its children".
     alias my-rsync='rsync --archive --verbose --delete --progress'
+end
+
+if test (hostname) = mur-pc-00705
+    set -gx ANDROID_HOME /usr/local/bin/android
+    set -gx ANDROID_SDK $ANDROID_HOME
+    set -gx ANDROID_NDK $ANDROID_HOME/ndk-bundle
+    set -gx ANDROID_NDK_ROOT $ANDROID_NDK
+    set -gx NDK_ROOT $ANDROID_NDK
+    set -gx VISUAL nvim
+    set -gx PATH $PATH $ANDROID_NDK $ANDROID_HOME/tools $ANDROID_HOME/platform-tools /opt/Imagination/PowerVR_Graphics/PowerVR_Tools/PVRTexTool/CLI/Linux_x86_64/
+
+    function my-cwebp
+        cwebp -m 6 -q 94 $argv[1] -o $argv[1]
+    end
 end
