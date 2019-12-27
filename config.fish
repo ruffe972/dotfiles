0de285a6c795bc1b5c2dc217fd748a0491182c60
Ivan Ershov <ivanershov51@gmail.com>
2019-12-27 17:12:19 +0300

    .

--- config.fish
+++ config.fish
@@ -10,34 +10,25 @@ alias set-def-perm-rec='set-def-file-perm-rec;set-def-dir-perm-rec'
 # git
 alias gch='git checkout'
 alias gst='git status'
-alias gbr='git branch'
 alias gsh='git stash'
 alias gcm='git commit'
+alias gdf='git diff'
+alias gdfs='git diff --staged'
 
-set -gx VISUAL micro
-set -gx EDITOR $VISUAL
+function my-cwebp
+    cwebp -m 6 -q 94 $argv[1] -o $argv[1]
+end
 
 if test (uname) = Linux
     alias open='xdg-open'
     alias trash='gio trash'
     alias clip-cp='xclip -selection clipboard'
     alias cp-path='echo -n (pwd) | clip-cp'
+end
 
+if test (hostname) = ivan-laptop
     # Overwrite local_dir with remote_dir:
     # my-rsync remote_host_name:remote_dir/ local_dir
     # Slash is important! It means "Copy not the dir itself, but its children".
     alias my-rsync='rsync --archive --verbose --delete --progress'
-end
-
-if test (hostname) = mur-pc-00705
-    set -gx ANDROID_HOME /usr/local/bin/android
-    set -gx ANDROID_SDK $ANDROID_HOME
-    set -gx ANDROID_NDK $ANDROID_HOME/ndk-bundle
-    set -gx ANDROID_NDK_ROOT $ANDROID_NDK
-    set -gx NDK_ROOT $ANDROID_NDK
-    set -gx PATH $PATH $ANDROID_NDK $ANDROID_HOME/tools $ANDROID_HOME/platform-tools /opt/Imagination/PowerVR_Graphics/PowerVR_Tools/PVRTexTool/CLI/Linux_x86_64/
-
-    function my-cwebp
-        cwebp -m 6 -q 94 $argv[1] -o $argv[1]
-    end
 end
---
+++ dash_config.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export PATH="$HOME/.cargo/bin:$PATH"
+export VISUAL=micro
+export EDITOR="$VISUAL"
+
+if [ $(hostname) = "mur-pc-00705" ]
+then
+    # android
+    export ANDROID_HOME=/usr/local/bin/android
+    export ANDROID_SDK="$ANDROID_HOME"
+    export ANDROID_NDK="$ANDROID_HOME/ndk-bundle"
+    export ANDROID_NDK_ROOT="$ANDROID_NDK"
+    export NDK_ROOT="$ANDROID_NDK"
+
+    export PATH="$PATH:$ANDROID_NDK:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:/opt/ImaginationPowerVR_Graphics/PowerVR_Tools/PVRTexTool/CLI/Linux_x86_64/"
+fi
