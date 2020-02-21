93043d35ee93b7b432655d09f149780d0e63694c
Ivan Ershov <ivanershov51@gmail.com>
2020-02-21 18:19:52 +0300

    vscodevim autoswitch input

---
+++ gnome_set_input_method.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval "imports.ui.status.keyboard.getInputSourceManager().inputSources[$1].activate()"
