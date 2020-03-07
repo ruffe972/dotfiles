76ca3a975396868c68f53d3a0aa195abba870548
Ivan Ershov <ivanershov51@gmail.com>
2020-03-07 19:19:58 +0300

    .

--- dnf_user_installed.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-dnf repoquery --userinstalled --qf "%{name}" > /tmp/dnf_list_installed
-grep -F -x -v -f "$DOTFILES/dnf_user_installed_blacklist.txt" /tmp/dnf_list_installed
-rm /tmp/dnf_list_installed
