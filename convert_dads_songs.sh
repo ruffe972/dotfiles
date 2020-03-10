2e05f5e9db170fa81d76b802cda469e44360ae0e
Ivan Ershov <ivanershov51@gmail.com>
2020-03-10 18:59:52 +0300

    .

--- convert_dads_songs.sh
+++ convert_dads_songs.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-echo todo
+echo todo test me
 exit
 
 set -e  # exit immediately if a command exits with a non-zero status
-PDF_DIR=аккорды_pdf
 
-cp -r "$1" .
-mv "$(basename "$1")" $PDF_DIR
-cd $PDF_DIR
-find . -iname '*.odt' \
+rm -rf аккорды_pdf
+cp -r "$1" аккорды_pdf
+find аккорды_pdf -iname '*.odt' \
     -execdir libreoffice --convert-to pdf {} \; \
     -delete
