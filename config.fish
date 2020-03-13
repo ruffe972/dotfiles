4f0e2f18074379f15b65e301ca1b3a3659be64e5
Ivan Ershov <ivanershov51@gmail.com>
2020-03-13 18:59:32 +0300

    .

--- config.fish
+++ config.fish
@@ -8,6 +8,9 @@ set -x EDITOR $VISUAL
 
 set PATH $PATH $HOME/.cargo/bin  # Modifying existing PATH var, so -x is not needed
 if test (hostname) = mur-pc-00705
+    # Haskell Stack
+    set PATH $PATH $HOME/.local/bin
+
     set PATH $PATH $ANDROID_NDK $ANDROID_HOME/tools $ANDROID_HOME/platform-tools /opt/ImaginationPowerVR_Graphics/PowerVR_Tools/PVRTexTool/CLI/Linux_x86_64/
 end
 source ~/dotfiles/functions.fish
