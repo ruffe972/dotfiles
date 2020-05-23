562c73ea211c224ba749f6225de5e661a797f963
Ivan Ershov <ivanershov51@gmail.com>
2020-05-23 17:51:36 +0300

    win term

--- scripts/windows_install.ps1
+++ scripts/windows_install.ps1
@@ -1,2 +1,3 @@
 Set-Variable dotfiles -Option Constant -Value C:\Users\Ivan\f\dotfiles
 New-Item -Force -Path C:\Users\Ivan\AppData\Roaming\Code\User\settings.json -ItemType SymbolicLink -Value $dotfiles\vscode_settings.json
+New-Item -Force -Path C:\Users\Ivan\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -ItemType SymbolicLink -Value $dotfiles\windows_terminal.json
