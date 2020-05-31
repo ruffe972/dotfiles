e7cba6f9dfdd206dc4e5dd233a6d7da9f2580e68
Ivan Ershov <ivanershov51@gmail.com>
2020-05-31 17:32:18 +0300

    .

--- scripts/choose_activity.kts
+++ scripts/choose_activity.kts
@@ -1,31 +1,28 @@
 val GAMING = "gaming"
 val ACTIVITIES = List(3) { GAMING } + List(1) { "movies" }
-val COMMON_VIDEOGAMES = listOf(
+val MULTIPLAYER_VIDEOGAMES = listOf(
     "Mario 3",
-    "Keep Talking and Nobody Explodes",
+    // "Keep Talking and Nobody Explodes",
     "Overcooked",
     "New Mario",
     "New Luigi",
     "Super Mario Party",
     "Minecraft",
-    "Pokken Tournament DX",
-    "Just Dance"
+    "Pokken Tournament DX"
 )
-
-// Also contains games in progress with both single and multiplayer.
-val MY_VIDEOGAMES = listOf(
+val SINGLE_AND_MULTIPLAYER_VIDEOGAMES = listOf(
     "Mario Kart DS",
     //"Witcher 2",
     "Mario Kart 8",
-    "Worms W.M.D"
+    "Worms W.M.D",
+    "Super Mario Maker 2"
 )
-
 val TABLETOP_GAMES = listOf(
     "MtG",
     "Carcassonne",
     "Dominion"
 )
-val ALL_GAMES = COMMON_VIDEOGAMES + MY_VIDEOGAMES + TABLETOP_GAMES
+val ALL_GAMES = MULTIPLAYER_VIDEOGAMES + SINGLE_AND_MULTIPLAYER_VIDEOGAMES + TABLETOP_GAMES
 
 val activity = ACTIVITIES.random()
 println(activity)
