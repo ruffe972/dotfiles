b728fcd83ee77d91a36c940e429fc3ff72ec188a
Ivan Ershov <ivanershov51@gmail.com>
2020-06-02 02:18:51 +0300

    .

--- scripts/choose_activity.kts
+++ scripts/choose_activity.kts
@@ -4,6 +4,7 @@ val MULTIPLAYER_VIDEOGAMES = listOf(
     "Mario 3",
     // "Keep Talking and Nobody Explodes",
     "Overcooked",
+    // "Overcooked 2",
     "New Mario",
     "New Luigi",
     "Super Mario Party",
@@ -11,11 +12,9 @@ val MULTIPLAYER_VIDEOGAMES = listOf(
     "Pokken Tournament DX"
 )
 val SINGLE_AND_MULTIPLAYER_VIDEOGAMES = listOf(
-    "Mario Kart DS",
     //"Witcher 2",
     "Mario Kart 8",
-    "Worms W.M.D",
-    "Super Mario Maker 2"
+    "Worms W.M.D"
 )
 val TABLETOP_GAMES = listOf(
     "MtG",
