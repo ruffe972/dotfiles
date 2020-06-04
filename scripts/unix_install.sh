dedcd2499d139aea9041ca68cae03eee3c910e7a
Ivan Ershov <ivanershov51@gmail.com>
2020-06-04 16:39:13 +0300

    pop os

--- scripts/unix_install.sh
+++ scripts/unix_install.sh
@@ -12,7 +12,7 @@ _create_links()
 
     ln -sf "$dotfiles/config.fish" ~/.config/fish/config.fish
     ln -sfT "$dotfiles/fish_functions" ~/.config/fish/functions
-    ln -sf "$dotfiles/vscode_settings.json" ~/.config/Code/User/settings.json
+    ln -sf "$dotfiles/vscodium_settings.json" ~/.config/VSCodium/User/settings.json
     ln -sf "$dotfiles/mpv_input.conf" ~/.config/mpv/input.conf
 
     if test "$(hostname)" = home-linux-desktop
@@ -23,12 +23,6 @@ _create_links()
 
 if test -x "$(command -v gsettings)"
 then
-    # Unmap Ctrl+Shift+Alt+Up/Down
-    gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Super><Shift>Page_Down']"
-    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Page_Down']"
-    gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Super><Shift>Page_Up']"
-    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Page_Up']"
-
     gsettings set org.gnome.desktop.peripherals.keyboard delay 250
 fi
 
