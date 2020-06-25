c4c660fa511fc0d04849340070599dcec6783852
Ivan Ershov <ivanershov51@gmail.com>
2020-06-26 01:22:53 +0300

    remove win install script

--- scripts/windows_install.ps1
+++
@@ -1,3 +0,0 @@
-# Run as admin
-Set-Variable dotfiles -Option Constant -Value C:\Users\Ivan\f\dotfiles
-New-Item -Force -Path C:\Users\Ivan\AppData\Roaming\VSCodium\User\settings.json -ItemType SymbolicLink -Value $dotfiles\vscodium_settings.json
