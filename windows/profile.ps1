7efc9e665485de54454baaacb7950d72fe19dfec
Ivan Ershov <ivanershov51@gmail.com>
2021-05-18 18:33:30 +0300

    Fix git alias

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -2,9 +2,7 @@ function cp-path {
     Set-Clipboard (Get-Location).Path
 }
 
+Set-Alias -Name g -Value git
 Import-Module posh-git
 
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
-# Aliases
-Set-Alias -Name g -Value git
